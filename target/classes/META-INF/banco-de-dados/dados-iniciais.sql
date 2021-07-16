3088591d539d21af360173334100631df2888d6b
cunhaDev <dev.mateuscunha@gmail.com>
2021-07-15 20:43:21 -0400

    feat: configuração inicial realizada

---
+++ target/classes/META-INF/banco-de-dados/dados-iniciais.sql
@@ -0,0 +1,1 @@
+insert into Produto (id, nome, descricao) values (1, 'Kindle', 'Conheça o novo Kindle, agora com iluminação embutida ajustável, que permite que você leia em ambientes abertos ou fechados, a qualquer hora do dia.');
